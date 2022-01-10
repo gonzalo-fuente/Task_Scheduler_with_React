711a4891e8fa8d228d1da3385c6eab324a36b35f
gonzalo <gonzalo_a_fuente@hotmail.com>
2022-01-10 02:34:44 -0300

    Initial commit - Frontend and backend finished The Frontend and backend are functional. Backend implemented with JSON Server.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h3>Simple Task Scheduler</h3>
+      <p>Using React and JSON Server</p>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
